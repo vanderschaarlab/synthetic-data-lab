024ad610823e92b69044178d7ed6c42481a02035
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-02-01 14:10:53 +0200

    Workflows (#2)
    * debug
    
    * cleanup
    
    * disable all tests
    
    * move models to LFS
    
    * debug

--- tests/nb_eval.py
+++ tests/nb_eval.py
@@ -24,18 +24,42 @@ def run_notebook(notebook_path: Path) -> None:
     proc.preprocess(nb, {"metadata": {"path": Tutorials_dir}})
 
 
-@click.command()
-@click.option("--nb", type=str, default=".")
-def main(nb: Path) -> None:
-    start = time()
-    try:
-        run_notebook(nb)
-    except BaseException as e:
-        print("FAIL", nb, e)
+enabled_tests = [
+    "Case Study 1 - Data Modality",
+]
 
-        raise e
-    finally:
-        print(f"Tutorial {nb} tool {time() - start}")
+
+@click.command()
+@click.option("--nb_dir", type=str, default=".")
+def main(nb_dir: Path) -> None:
+    start = time()
+
+    nb_dir = Path(nb_dir)
+
+    for nb in nb_dir.rglob("*"):
+        if nb.suffix != ".ipynb":
+            continue
+
+        if "checkpoint" in nb.name:
+            continue
+
+        enabled = False
+        for enabled_test in enabled_tests:
+            if enabled_test in nb.name:
+                enabled = True
+
+        if not enabled:
+            continue
+
+        print("Testing", nb.name)
+        try:
+            run_notebook(nb)
+        except BaseException as e:
+            print("FAIL", nb, e)
+
+            raise e
+        finally:
+            print(f"Tutorial {nb} tool {time() - start}")
 
 
 if __name__ == "__main__":
